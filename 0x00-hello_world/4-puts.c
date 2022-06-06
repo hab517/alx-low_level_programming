faf307faaea30ceeac6dbeeebc9bf7301466398c
hab517 <hab51721@gmail.com>
2022-06-06 13:58:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*main.c*/
+/**
+ * main-prints the size of various types
+ * Return:0 if exited properly,non-zero otherwise
+ */
 int main(void)
 {
 puts("\"programming is like building a multilingual puzzle");
